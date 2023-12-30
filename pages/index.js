4df51f662fd9e8607b939f3eb82679103d766b41
Seunghyeok Lee <seunghyeok.tech@gmail.com>
2023-12-30 20:45:18 +0900

    Movie Detail

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,6 @@
 import Seo from "@/components/Seo";
+import Link from "next/link";
+import { useRouter } from "next/router";
 
 export async function getServerSideProps() {
   const { results } = await (
@@ -8,13 +10,42 @@ export async function getServerSideProps() {
 }
 
 export default function Home({ results }) {
+  const router = useRouter();
+  const onClick = (id, title) => {
+    router.push(
+      {
+        pathname: `/movies/${id}`,
+        query: {
+          title,
+        },
+      },
+      `/movies/${id}`
+    );
+  };
+
   return (
     <div className="container">
       <Seo title="Home" />
       {results?.map((movie) => (
-        <div className="movie" key={movie.id}>
+        <div
+          onClick={() => onClick(movie.id, movie.original_title)}
+          className="movie"
+          key={movie.id}
+        >
           <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} />
-          <h4>{movie.original_title}</h4>
+          <h4>
+            <Link
+              href={{
+                pathname: `/movies/${movie.id}`,
+                query: {
+                  title: movie.original_title,
+                },
+              }}
+              as={`/movies/${movie.id}`}
+            >
+              {movie.original_title}
+            </Link>
+          </h4>
         </div>
       ))}
       <style jsx>{`
--- pages/movies/[id].js
+++ pages/movies/[id].js
@@ -2,6 +2,9 @@ import { useRouter } from "next/router";
 
 export default function Detail() {
   const router = useRouter();
-
-  return <p>Movie ID: {router.query.id}</p>;
+  return (
+    <div>
+      <h4>{router.query.title || "Loading..."}</h4>
+    </div>
+  );
 }
